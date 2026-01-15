1f3d4faf2beff0cd9ce7562284bc5c86b99661aa
Alexandre Junior <souzajr.alexandre@gmail.com>
2026-01-14 21:48:27 -0300

    Codigo-fonte da Aula 02

--- procardio-api/src/main/java/br/com/procardio/api/controller/UsuarioController.java
+++ procardio-api/src/main/java/br/com/procardio/api/controller/UsuarioController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +24,13 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @PostMapping
     public ResponseEntity<Usuario> cadastrarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO) {
         Usuario novoUsuario = usuarioService.salvarUsuario(usuarioDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoUsuario);
     }
 
+    @PutMapping("/{id}")
     public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @Valid @RequestBody UsuarioDTO usuarioDTO) {
         Usuario usuarioAtualizado = usuarioService.salvarUsuario(id, usuarioDTO);
         
--- procardio-api/src/main/java/br/com/procardio/api/model/Usuario.java
+++ procardio-api/src/main/java/br/com/procardio/api/model/Usuario.java
@@ -1,5 +1,13 @@
 package br.com.procardio.api.model;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import br.com.procardio.api.dto.UsuarioDTO;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
@@ -15,7 +23,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "tb_usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -51,6 +59,41 @@ public class Usuario {
         }
 
         return usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
